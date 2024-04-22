c9570c9678c61def2b9ff154cc9a27536525ff31
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-04-22 22:31:35 +0200

    mise en application du cours telechargement de tous les package, oragnisation du backend (models route controllers middelware) mise en place fonction SignIn et Login ajout de livre suppresion etc etc

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+const stuffRoutes = require("./routes/stuff.js");
+const userRoutes = require("./routes/user.js");
+const path = require('path');
+
+mongoose.connect("mongodb+srv://benjaminmazars:TMDmbXohG7NjlMa7@projet6.ijnp1yx.mongodb.net/?retryWrites=true&w=majority&appName=Projet6")
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use(bodyParser.json());
+
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+
+module.exports = app; 
